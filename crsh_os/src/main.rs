ddefc7c32713cf4941b389925078cde928f0ad22
Lily <ngross1015@gmail.com>
2026-08-19 11:41:29 -0700

    Basic entrypoint added

--- crsh_os/src/main.rs
+++ crsh_os/src/main.rs
@@ -1,9 +1,15 @@
 #![no_std]
+#![no_main]
 
 use core::panic::PanicInfo;
 
 //called on panic
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[unsafe)no_mangle)]
+pub extern "C" fn _start() -> ! {
     loop {}
 }
