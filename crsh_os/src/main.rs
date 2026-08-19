f8acf42deeba477f1a111d06a63bd8a372eee194
Lily <ngross1015@gmail.com>
2026-08-19 13:42:46 -0700

    Implement basic VGA buffer

--- crsh_os/src/main.rs
+++ crsh_os/src/main.rs
@@ -9,7 +9,18 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
+static HELLO: &[u8] = b"Hello World!";
+
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! { //custom entry point
+    let vga_buffer = 0xb800 as *mut u8; //create VGA buffer at address 0xb800
+
+    for (i, &byte) in HELLO.iter().enumerate() { //iterate through the HELLO variable for bytes
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte; //write byte
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb; //set byte color
+        }
+    }
+
     loop {}
 }
