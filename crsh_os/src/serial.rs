36939fd83f19a0ee8a807584a81d9e104b3b6cbf
Lily <ngross1015@gmail.com>
2026-08-19 15:15:00 -0700

    Add serialPrintr and serialLnprintr

--- crsh_os/src/serial.rs
+++ crsh_os/src/serial.rs
@@ -7,3 +7,25 @@ lazy_static! {
         Uart16550Tty::new_port(0x3F8, Config::default()).expect("Failed to initialize UART.")
     });
 }
+
+#[doc(hidden)]
+pub fn _print(args: ::core::fmt::Arguments) {
+    use core::fmt::Write;
+    SERIAL1.lock().write_fmt(args).expect("Printing to serial failed.");
+}
+
+//print to hos through serial interface
+#[macro_export]
+macro_rules! serialPrintr {
+    ($($arg:tt)*) => {
+        $crate::serial::_print(format_args!($($arg)*));
+    };
+}
+
+//print to host through serial interface and append newline
+#[macro_export]
+macro_rules! serialLnprintr {
+    () => ($crate::serialPrintr!("\n"));
+    ($fmt:expr) => ($crate::serialPrintr!(concat!($fmt, "\n")));
+    ($fmt:expr, $($arg:tt)*) => ($crate::serialPrintr!(concat!($fmt, "\n"), $($arg)*));
+}
