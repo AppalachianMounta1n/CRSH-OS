0ca8fd39c31b131f35eecbf93069f2a11d4e1baa
Lily <ngross1015@gmail.com>
2026-08-19 15:47:58 -0700

    Relocate basic_boot.rs to correct location

--- crsh_os/.cargo/tests/basic_boot.rs
+++ crsh_os/.cargo/tests/basic_boot.rs
@@ -1,7 +1,7 @@
 #![no_std]
 #![no_main]
 #![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
+#![test_runner(crsh_os::test_runner)]
 #![reexport_test_harness_main = "testMain"]
 
 use core::panic::PanicInfo;
@@ -10,27 +10,15 @@ use crsh_os::lnprintr;
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
     testMain();
-
     loop {}
 }
-
-fn test_runner(tests: &[&dyn Fn()]) {
-    unimplemented!();
-}
-
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    loop {}
-}
-
-#![test_runner(crsh_os::test_runner)]
 
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     crsh_os::testPanicHandler(info)
 }
 
-#[test-case]
+#[test_case]
 fn testLnprintr() {
     lnprintr!("Test lnprintr output.");
 }
