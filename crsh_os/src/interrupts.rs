fe7fb7ff92913d60070239e87f65dff02b119a8b
Lily <ngross1015@gmail.com>
2026-08-26 17:11:48 -0700

    idt loading

--- crsh_os/src/interrupts.rs
+++ crsh_os/src/interrupts.rs
@@ -1,9 +1,10 @@
-use x86_64::structures::idt::InterruptDescriptorTable;
+use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
 use crate::lnprintr;
 
 pub fn init_idt() { //initialize interrupt descriptor table
     let mut idt = InterruptDescriptorTable::new();
     idt.breakpoint.set_handler_fn(breakpointHandler);
+    idt.load(); //load interrupt descriptor table
 }
 
 extern "x86-interrupt" fn breakpointHandler(stackFrame: InterruptStackFrame) {
