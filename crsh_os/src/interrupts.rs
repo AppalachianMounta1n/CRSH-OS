415e0646147d40874415685f14937ebf04bb75ad
Lily <ngross1015@gmail.com>
2026-08-26 17:30:18 -0700

    Remove unsafe into abstractions with lazy_static

--- crsh_os/src/interrupts.rs
+++ crsh_os/src/interrupts.rs
@@ -1,13 +1,17 @@
 use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
 use crate::lnprintr;
+use lazy_static::lazy_static;
 
-static mut IDT: InterruptDescriptorTable = InterruptDescriptorTable::new();
+lazy_static! { //avoid direct unsafe code utilizing lazy_static abstractions
+    static ref IDT: InterruptDescriptorTable = {
+        let mut idt = InterruptDescriptorTable::new();
+        idt.breakpoint.set_handler_fn(breakpointHandler);
+        idt
+    };
+}
 
 pub fn init_idt() { //initialize interrupt descriptor table
-    unsafe { //this is not a good way to do it but it works probably
-        IDT.breakpoint.set_handler_fn(breakpointHandler);
-        IDT.load(); //load interrupt descriptor table
-    }
+    IDT.load(); //load interrupt descriptor table
 }
 
 extern "x86-interrupt" fn breakpointHandler(stackFrame: InterruptStackFrame) {
